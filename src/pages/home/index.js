12f3586d95754adfaf332592a7f1dac22d1bcef7
FabianoBS <fabianobrums@gmail.com>
2024-10-20 19:40:42 -0300

    adjust ';' media query

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import './styles.css'
 import Header from "../../components/header/header";
 import Trailer from "../../components/trailer/trailer";
 import Cards from "../../components/cards";
 import Footer from "../../components/footer/footer";
+import './styles.css'
 
 function Home(){
     return (
