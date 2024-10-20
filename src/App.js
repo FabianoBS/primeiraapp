2256a4610c0162baba13c93d613844916921e7c7
FabianoBS <fabianobrums@gmail.com>
2024-10-20 18:21:58 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import React from 'react';
 import Home from './pages/home';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Contato from './pages/contato/contato';
 import Fotos from './pages/fotos/fotos';
 import Comentarios from './pages/comentarios/comentarios';
 import Detalhes from './pages/detalhes/detalhes';
 
-
 function App() {
-
   return (
-    <Router>
+    <Router basename="/primeiraapp">
       <Routes>
-        <Route path='/' element={<Home />} /> 
-        <Route path='/primeiraapp' element={<Home />} />
-        <Route path='/contato' element={<Contato />} />
-        <Route path='/fotos' element={<Fotos />} />
-        <Route path='/comentarios' element={<Comentarios />} />
-        <Route path='/detalhes/:id' element={<Detalhes />} />
+        <Route path="/" element={<Home />} /> 
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/fotos" element={<Fotos />} />
+        <Route path="/comentarios" element={<Comentarios />} />
+        <Route path="/detalhes/:id" element={<Detalhes />} />
       </Routes>
     </Router>
   );
